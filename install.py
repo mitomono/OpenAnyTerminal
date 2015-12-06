b35d6d0ead6b5cdd662fd16fb127048425f6597e
mitomono <mitomono@gmail.com>
2015-12-06 13:07:10 +0100

    actualizacion del setup

--- install.py
+++ install.py
@@ -1,10 +1,18 @@
 #! /usr/bin/python
 
+import shutil, os, getpass
+
+user = getpass.getuser()
+orig = './OpenAnyTerminal.py'
+dest = '/home/'+user+'/.local/share/nautilus-python/extensions/OpenAnyTerminal.py'
+
 # Read de command for launch the terminal
 command_term = raw_input('introduce el comando para abrir el terminal deseado: ')
 
+shutil.copy(orig, dest)
+
 # Open file extension
-f = open("./open-any-terminal.py", 'r')
+f = open(orig, 'r')
 
 # Read full file, save the text and replace the string "comando" by command_term
 texto = f.read()
@@ -14,6 +22,6 @@ texto = texto.replace("comando", command_term)
 f.close()
 
 # Reopen the file and writes the changed text
-salida = open("./open-any-terminal.py", 'w')
+salida = open(dest, 'w')
 salida.write(texto)
 salida.close()
