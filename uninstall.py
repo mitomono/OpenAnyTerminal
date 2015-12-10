d6270e007dd7fca71fbfc77e5b8d00be6c76fc67
Adrián Lara Roldán <mitomono@users.noreply.github.com>
2015-12-10 01:07:25 +0100

    Delete uninstall.py

--- uninstall.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python
-
-import os, getpass
-
-user = getpass.getuser()
-
-os.remove('/home/'+user+'/.local/share/nautilus-python/extensions/OpenAnyTerminal.pyc')
-os.remove('/home/'+user+'/.local/share/nautilus-python/extensions/OpenAnyTerminal.py')
